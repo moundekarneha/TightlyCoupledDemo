4968454ef0d3e54837eb1024dd764f13a92f7a1b
HP <HP@DESKTOP-G4J7E8F>
2022-07-08 18:22:34 +0530

    Add project files.

---
+++ TightlyCoupledDemo/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TightlyCoupledDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        
+    }
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } 
+        public string Standard { get; set; }
+    }
+
+    public class StudentDataAccessLayer
+    {
+      public List<Student> GetStudents()
+        {
+            List<Student> students = new List<Student>();
+            students.Add(new Student()
+            {
+                Id = 1,
+                Name = "Neha",
+                Standard = "A"
+            });
+
+            students.Add(new Student()
+            {
+                Id = 2,
+                Name = "Sneha",
+                Standard = "A"
+            });
+
+            students.Add(new Student()
+            {
+                Id = 3,
+                Name = "Nehal",
+                Standard = "A"
+            });
+
+            students.Add(new Student()
+            {
+                Id = 4,
+                Name = "Himanshu",
+                Standard = "B"
+            });
+
+            students.Add(new Student()
+            {
+                Id = 5,
+                Name = "Kunal",
+                Standard = "A"
+            });
+            return students;
+        }
+        
+    }
+
+
+    //below class is highly dependent on StudentDataAccessLayer --Tightly coupled example
+    public class StudentBussinessLayer
+    {
+        public List<Student> GetStudents()
+        {
+            StudentDataAccessLayer obj = new StudentDataAccessLayer();
+            return obj.GetStudents();
+        }
+    }
+}
